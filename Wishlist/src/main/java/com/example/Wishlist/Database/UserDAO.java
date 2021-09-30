27ba0366d4bc8a43cbd5be325b78b27cced4157a
nmcmahan <nmcmahan@csumb.edu>
2021-09-30 00:49:22 -0700

    Created database classes, and started on the DAO Queries. Now if you run the WishlistApplication file, it should create a database when stopped.

---
+++ Wishlist/src/main/java/com/example/Wishlist/Database/UserDAO.java
@@ -0,0 +1,10 @@
+package com.example.Wishlist.Database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserDAO extends JpaRepository<UserDB, Long> {
+
+    @Query("FROM UserDB WHERE username = ?1")
+    UserDB findUserByname(String username);
+}
