e3aa1490b7cd76c78dedfe3fe6aa3b2c21f79755
nmcmahan <nmcmahan@csumb.edu>
2021-10-05 12:14:54 -0700

    Created some more queries within the DAO files that we migh need later. ALso, removed sping security so we can avoid the lgin page when oing to localhost, tested the /hello endpoint, and it displayed properly

--- Wishlist/src/main/java/com/example/Wishlist/Database/ItemDAO.java
+++ Wishlist/src/main/java/com/example/Wishlist/Database/ItemDAO.java
@@ -9,4 +9,10 @@ public interface ItemDAO extends JpaRepository<ItemDB, Long> {
 
     @Query("FROM ItemDB WHERE user_id = ?1")
     List<ItemDB> findItemByUser(int user_id);
+
+    @Query("FROM ItemDB WHERE list_name = ?1")
+    List<ItemDB> findItemByList(String list_name);
+
+    @Query("FROM ItemDB WHERE name = ?1 OR id = ?1")
+    List<ItemDB> findItemBySearch(String entry);
 }
