1c9c5d62f6366b79f4adb59ae8190d6c88f99cc2
James Campbell <jscampbell43@gmail.com>
2021-10-18 23:24:45 -0700

    Update UserDAO.java

--- src/main/java/com/example/Wishlist/Database/UserDAO.java
+++ src/main/java/com/example/Wishlist/Database/UserDAO.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.Modifying;
 // import org.springframework.data.jpa.repository.Transactional;
-import org.springframework.data.jpa.repository.Param;
+// import org.springframework.data.jpa.repository.Param;
 
 
 public interface UserDAO extends JpaRepository<UserDB, Long> {
@@ -12,8 +12,8 @@ public interface UserDAO extends JpaRepository<UserDB, Long> {
     @Query("FROM UserDB WHERE username = ?1")
     UserDB findUserByname(String username);
 
-    @Modifying
-    @Query(value = "INSERT into UserDB (username,password) VALUES (:username,:password)", nativeQuery = true)
-    UserDB insertUser(@Param("username") String username, @Param("password") String password);
+    // @Modifying
+    // @Query(value = "INSERT into UserDB (username,password) VALUES (:username,:password)", nativeQuery = true)
+    // UserDB insertUser(@Param("username") String username, @Param("password") String password);
 
 }
