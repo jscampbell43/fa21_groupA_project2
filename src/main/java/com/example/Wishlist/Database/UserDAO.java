32cbaebaff9f30093440ad8689ba86dd78a903ba
James Campbell <jscampbell43@gmail.com>
2021-10-19 11:29:20 -0700

    Update UserDAO.java

--- src/main/java/com/example/Wishlist/Database/UserDAO.java
+++ src/main/java/com/example/Wishlist/Database/UserDAO.java
@@ -13,7 +13,7 @@ public interface UserDAO extends JpaRepository<UserDB, Long> {
     UserDB findUserByname(String username);
 
     @Modifying
-    @Query("INSERT into UserDB (username,password) VALUES (:username,:password)", nativeQuery = true)
+    @Query("INSERT into UserDB (username,password) VALUES (username,password)", nativeQuery = true)
     UserDB insertUser(String username, String password);
 
 }
