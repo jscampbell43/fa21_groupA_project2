455008cbf34f9f66641b2b645198bbbeb58e0698
James Campbell <jscampbell43@gmail.com>
2021-10-18 20:08:37 -0700

    Update UserController.java

--- src/main/java/com/example/Wishlist/controllers/UserController.java
+++ src/main/java/com/example/Wishlist/controllers/UserController.java
@@ -19,6 +19,6 @@ public class UserController {
     public List<UserDB> getAllUsers(){return userDAO.findAll();}
 
     @GetMapping(value = "/signup")
-    public void insertNewUser(){userDAO.insertUser(); return true;}
+    public void insertNewUser(){userDAO.insertUser();}
     
 }
