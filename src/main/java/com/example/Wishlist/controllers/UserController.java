03205dcbad7f7e5da0931e79923b49affcb63c49
James Campbell <jscampbell43@gmail.com>
2021-10-19 11:24:52 -0700

    Update UserController.java

--- src/main/java/com/example/Wishlist/controllers/UserController.java
+++ src/main/java/com/example/Wishlist/controllers/UserController.java
@@ -2,9 +2,7 @@ package com.example.Wishlist.controllers;
 
 import com.example.Wishlist.Database.UserDAO;
 import com.example.Wishlist.Database.UserDB;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
