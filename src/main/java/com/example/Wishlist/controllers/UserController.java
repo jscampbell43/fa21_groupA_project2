deef35fd4c6dca4783a30d54a6dbb96133495bee
James Campbell <jscampbell43@gmail.com>
2021-10-20 13:09:44 -0700

    Update UserController.java

--- src/main/java/com/example/Wishlist/controllers/UserController.java
+++ src/main/java/com/example/Wishlist/controllers/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
     }
     @PostMapping("/saveItemToWishlist")
     public void saveItemToWishlist(HttpServletResponse response, ) {
-        UserDB  user = userDAO.findUserByUserId()
+        UserDB  user = userDAO.findUserByUserId();
     }
 
 
