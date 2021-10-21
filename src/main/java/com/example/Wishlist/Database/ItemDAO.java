6fa57adf5800a29ce486ebc721b1c7165255cdfa
nmcmahan <nmcmahan@csumb.edu>
2021-10-20 19:23:33 -0700

    Got an insert query working, not quite ready to pull, but pushing so that the code is up on github

--- src/main/java/com/example/Wishlist/Database/ItemDAO.java
+++ src/main/java/com/example/Wishlist/Database/ItemDAO.java
@@ -1,8 +1,11 @@
 package com.example.Wishlist.Database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ItemDAO extends JpaRepository<ItemDB, Long> {
@@ -15,4 +18,9 @@ public interface ItemDAO extends JpaRepository<ItemDB, Long> {
 
     @Query("FROM ItemDB WHERE name = ?1 OR id = ?1")
     List<ItemDB> findItemBySearch(String entry);
+
+    @Modifying
+    @Query(value = "INSERT INTO ITEMS (user_id, name, description, item_link, img_url, List_name) VALUES (:uid,:name,:description,:link,:url,:list)", nativeQuery = true)
+    @Transactional
+    void addItem(@Param("uid") int user_id, @Param("name") String name, @Param("description") String desc, @Param("link") String link, @Param("url") String url, @Param("list") String list);
 }
--- src/main/java/com/example/Wishlist/controllers/ItemController.java
+++ src/main/java/com/example/Wishlist/controllers/ItemController.java
@@ -4,10 +4,7 @@ package com.example.Wishlist.controllers;
 import com.example.Wishlist.Database.ItemDAO;
 import com.example.Wishlist.Database.ItemDB;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collector;
@@ -37,6 +34,11 @@ public class ItemController {
 
     @RequestMapping(value = "/list/{list_name}/user/{user_id}")
     public List<ItemDB> getItemsByList(@PathVariable String list_name, @PathVariable int user_id) {return  itemDAO.findItemByList(list_name, user_id);}
+
+    @RequestMapping(value = "/additem")
+    public void addItem(@RequestParam int id, @RequestParam String name, @RequestParam String desc, @RequestParam String link, @RequestParam String image, @RequestParam String list) {
+        itemDAO.addItem(id, name, desc, link, image, list);
+    }
 
 //    @Query("FROM ItemDB WHERE user_id = ?1")
 //    List<ItemDB> findItemByUser(int user_id);
