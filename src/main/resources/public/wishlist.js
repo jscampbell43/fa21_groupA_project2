5ec964616ba26ae87b057284486550e30f57922e
nmcmahan <nmcmahan@csumb.edu>
2021-10-22 14:40:50 -0700

    Wishlist page now displays  all user items, and the search button can be used to filter by list name, with exception messages for empty lists

--- src/main/resources/public/wishlist.js
+++ src/main/resources/public/wishlist.js
@@ -7,13 +7,18 @@ $(document).ready(function(){
         success: function (data) {
            // alert("Call success");
             console.log(data);
+            if($(data).length<1) {
+                $("#wishlistsDiv").empty();
+                $("#wishlistsDiv").append("You don't appear to have any items, go add some!");
+            } else {
+                $("#wishlistsDiv").empty();
             $(data).each(function(){
-                console.log(this);
-                let itemInfo = '<p>Item id: '+this.id+' Item name: ' +this.name+' Item Link: <a href="'+this.item_link+'">'+this.item_link+'</a></p>';
+                let itemInfo = '<p>Item id: '+this.id+', Item name: ' +this.name+', Item Link: <a href="'+this.item_link+'">'+this.item_link+'</a></p>';
                 let itemImage = '<img src="'+this.img_url+'" alt="image"><br>';
                 let itemInfo2 = '<p>Description: '+this.description+'<br>List name: '+this.list_name+'</p><br><br>';
                 $("#wishlistsDiv").append(itemInfo, itemImage, itemInfo2);
             });
+            }
         },
         error: function () {
             $("#wishlistsDiv").empty();
@@ -29,13 +34,17 @@ $(document).ready(function(){
                 $("#wishlistsDiv").empty();
                 // alert("Call success");
                 console.log(data);
-                $(data).each(function () {
-                    console.log(this);
-                    let itemInfo = '<p>Item id: ' + this.id + ' Item name: ' + this.name + ' Item Link: <a href="' + this.item_link + '">' + this.item_link + '</a></p>';
-                    let itemImage = '<img src="' + this.img_url + '" alt="image"><br>';
-                    let itemInfo2 = '<p>Description: ' + this.description + '<br>List name: ' + this.list_name + '</p><br><br>';
-                    $("#wishlistsDiv").append(itemInfo, itemImage, itemInfo2);
-                });
+                if($(data).length<1) {
+                    $("#wishlistsDiv").empty();
+                    $("#wishlistsDiv").append("There don't seem to be any items in this list");
+                } else {
+                    $(data).each(function () {
+                        let itemInfo = '<p>Item id: ' + this.id + ' Item name: ' + this.name + ' Item Link: <a href="' + this.item_link + '">' + this.item_link + '</a></p>';
+                        let itemImage = '<img src="' + this.img_url + '" alt="image"><br>';
+                        let itemInfo2 = '<p>Description: ' + this.description + '<br>List name: ' + this.list_name + '</p><br><br>';
+                        $("#wishlistsDiv").append(itemInfo, itemImage, itemInfo2);
+                    });
+                }
             },
             error: function () {
                 $("#wishlistsDiv").empty();
